32e8ba7617c7fc44b6cf980e74fa40769431a3be
samuelscheit <github@samuelscheit.com>
2026-01-13 03:31:45 +0100

    fix: build

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
 	/* config options here */
 	reactCompiler: true,
 	serverExternalPackages: ["puppeteer-real-browser", "rebrowser-puppeteer-core"],
-	output: "export",
 };
 
 export default nextConfig;
